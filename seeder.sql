0462dfc24988408f6d179431b8b1c6d4427c570c
AliciaMonsibais <alicia.monsibais1@gmail.com>
2019-08-29 10:40:48 -0500

    resolving conflict

--- seeder.sql
+++ seeder.sql
@@ -1,8 +1,6 @@
 USE pokemon_adlister_db;
 
 INSERT INTO users(username,email,password)
-VALUES('Alex','test@gmail.com','pass');
-
 
 INSERT INTO  ads(user_id,title,description)
 VALUES(1,'Jigglypuff','Jigglypuff will not stop singing!Need to get rid of it!');
